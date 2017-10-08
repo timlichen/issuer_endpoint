acc13ddd4c634a46b4bd8b5c1c5d69fa384c4207
timlichen <tim.li.chen@gmail.com>
2017-10-08 12:43:39 -0400

    added issuer json data and changed route names

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route('/issuer')
+@app.route('/issuer.json')
 def issuer():
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
     json_url = os.path.join(SITE_ROOT, "templates/", "issuer.json")
     data = json.load(open(json_url))
     return jsonify(data)
 
-@app.route("/revocation")
+@app.route("/revocation.json")
 def revocation():
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
     json_url = os.path.join(SITE_ROOT, "templates/", "revocation.json")
