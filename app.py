39d5c46d1bda45b4d0a38d4b730d001ff310b487
timlichen <tim.li.chen@gmail.com>
2017-10-08 12:36:49 -0400

    changing route names

--- app.py
+++ app.py
@@ -3,23 +3,24 @@ from flask import Flask, request, render_template, jsonify, url_for, json
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
     return render_template("index.html")
 
-@app.route("/issuer")
-def index():
+@app.route('/issuer')
+def issuer():
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
     json_url = os.path.join(SITE_ROOT, "templates/", "issuer.json")
     data = json.load(open(json_url))
     return jsonify(data)
 
 @app.route("/revocation")
-def index():
+def revocation():
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
     json_url = os.path.join(SITE_ROOT, "templates/", "revocation.json")
     data = json.load(open(json_url))
     return jsonify(data)
 
 
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
