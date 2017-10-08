90c0ce202196296bfcfa0925d580cc41e066dbdf
timlichen <tim.li.chen@gmail.com>
2017-10-08 12:10:10 -0400

    first commit

---
+++ server.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, request, render_template, jsonify, url_for, json
+
+app = Flask(__name__)
+
+@app.route("/issuer")
+def index():
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "templates/", "issuer.json")
+    data = json.load(open(json_url))
+    return jsonify(data)
+
+@app.route("/revocation")
+def index():
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "templates/", "revocation.json")
+    data = json.load(open(json_url))
+    return jsonify(data)
+
+
+app.run()
